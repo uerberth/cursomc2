6c9ba8d90f3b6d9f1139aeb9e57a90fdf9687225
uerberth jancen <uerberthjancenpereira@gmail.com>
2019-07-25 11:46:51 -0400

    primeiro comit

---
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
+		return obj;
+	}
+
+}
