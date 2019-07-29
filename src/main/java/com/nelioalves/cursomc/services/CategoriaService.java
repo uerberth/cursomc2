2e2fdb06381fcf21bd2fe75260e5826dd496c6d7
uerberth <uerberthjancenpereira@gmail.com>
2019-07-29 09:19:18 -0400

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -35,9 +35,11 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
-	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+    public Categoria update(Categoria obj) {
+		
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -63,6 +65,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
 
