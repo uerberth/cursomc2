a24eb73f5238b14b0bea40e7ab0e8f0bf41b6c92
uerberth <uerberthjancenpereira@gmail.com>
2019-07-29 13:13:42 -0400

    validação customizada : email não repetido na inserção de Cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,6 +27,8 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique=true)// garante que no banco de dados não vai ter repetição nesse campo
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
--- src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
@@ -8,4 +8,6 @@ import com.nelioalves.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>  {
 
+	@org.springframework.transaction.annotation.Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
